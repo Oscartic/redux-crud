2582e0b89ae65be22245c97db6e07424b5bd238b
OscarB <oballesteros@abstract.cl>
2019-04-26 13:02:28 -0400

    Create components and config json-server

---
+++ src/componentes/EditarProducto.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EditarProducto extends Component {
+    render() {
+        return (
+            <h1>
+                Desde componente Editar Producto
+            </h1>
+        );
+    }
+}
+
+export default EditarProducto;
---
+++ src/componentes/NuevoProducto.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class NuevoProducto extends Component {
+   
+    render() { 
+        return ( <h1>Desde nuevo producto</h1> );
+    }
+}
+ 
+export default NuevoProducto;
