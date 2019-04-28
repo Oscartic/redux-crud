372cbb4b246610d220407805e1277ba0bfdae2ce
OscarB <oballesteros@abstract.cl>
2019-04-28 00:42:16 -0400

    Add component Producto and actions

---
+++ src/actions/productosActions.js
@@ -0,0 +1,19 @@
+import { MOSTAR_PRODUCTOS, MOSTRAR_PRODUCTO, MOSTRAR_PRODUCTOS, ELIMINAR_PRODUCTO } from './types';
+
+import axios from 'axios'; 
+
+export const mostrarProductos = () => async dispatch => {
+    const respuesta = await axios.get('http://localhost:5000/productos');
+    dispatch({
+        type: MOSTRAR_PRODUCTOS,
+        payload: respuesta.data
+    })
+}
+
+export const borrarProducto = (id) => async dispatch => {
+    await axios.delete(`http://localhost:5000/productos/${id}`);
+    dispatch({
+        type: ELIMINAR_PRODUCTO,
+        payload: id
+    });
+}
